ba3e4c64a2cbd228e3f75605d191a9e47ce4a42e
MiniCodehub <ironman102004@gmail.com>
2024-10-27 08:53:46 +0530

    icon.png

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,10 @@ import 'package:attendanceapp/pages/teacher_page.dart';
 import 'package:attendanceapp/services/helper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
