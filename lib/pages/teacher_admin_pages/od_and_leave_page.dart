01a3ba3b9ad496c524c4ddb8959efc1d6cd19be7
MiniCodehub <ironman102004@gmail.com>
2024-11-25 18:49:31 +0530

    done

--- lib/pages/teacher_admin_pages/od_and_leave_page.dart
+++ lib/pages/teacher_admin_pages/od_and_leave_page.dart
@@ -1,12 +1,12 @@
 import 'package:attendanceapp/core/theme/app_colors.dart';
 import 'package:attendanceapp/pages/teacher_admin_pages/view_permission_letter_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ODAndLeavePage extends StatefulWidget {
-  const ODAndLeavePage({super.key, required this.classDetails});
+  const ODAndLeavePage({super.key, this.classDetails});
 
-  final String classDetails;
+  final String? classDetails;
 
   @override
   State<ODAndLeavePage> createState() => _ODAndLeavePageState();
@@ -15,53 +15,56 @@ class ODAndLeavePage extends StatefulWidget {
 class _ODAndLeavePageState extends State<ODAndLeavePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection("odAndLeave")
-            .where("classdetails", isEqualTo: widget.classDetails)
-            .where("status", isNull: true)
-            .orderBy("date", descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.data!.docs.isEmpty) {
-            return const Center(
-              child: Text("No leave or OD permission applied."),
-            );
-          }
-
-          var data = snapshot.data?.docs;
-          return ListView.builder(
-            itemCount: data!.length,
-            itemBuilder: (context, index) {
-              Map data_ = data[index].data() as Map;
-              String sub = data_["sub"];
-              Timestamp timestamp = data_['date'] ?? Timestamp(1, 1);
-              DateTime dateTime = timestamp.toDate();
-              String date =
-                  "${dateTime.day}/${dateTime.month}/${dateTime.year}";
-              String content = data_["content"];
-              String name = data_["name"];
-              String rollno = data_["rollno"];
-
-              return Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: permissionTileWidget(
-                  id: data[index].id,
-                  name: name,
-                  rollno: rollno,
-                  content: content,
-                  sub: sub,
-                  date: date,
-                ),
-              );
-            },
-          );
-        },
+    return const Scaffold(
+      body: Center(
+        child: Text("No data found"),
       ),
+      // body: StreamBuilder(
+      //   stream: FirebaseFirestore.instance
+      //       .collection("odAndLeave")
+      //       .where("classdetails", isEqualTo: widget.classDetails)
+      //       .where("status", isNull: true)
+      //       .orderBy("date", descending: true)
+      //       .snapshots(),
+      //   builder: (context, snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.waiting) {
+      //       return const Center(child: CircularProgressIndicator());
+      //     }
+      //     if (snapshot.data!.docs.isEmpty) {
+      //       return const Center(
+      //         child: Text("No leave or OD permission applied."),
+      //       );
+      //     }
+
+      //     var data = snapshot.data?.docs;
+      //     return ListView.builder(
+      //       itemCount: data!.length,
+      //       itemBuilder: (context, index) {
+      //         Map data_ = data[index].data() as Map;
+      //         String sub = data_["sub"];
+      //         Timestamp timestamp = data_['date'] ?? Timestamp(1, 1);
+      //         DateTime dateTime = timestamp.toDate();
+      //         String date =
+      //             "${dateTime.day}/${dateTime.month}/${dateTime.year}";
+      //         String content = data_["content"];
+      //         String name = data_["name"];
+      //         String rollno = data_["rollno"];
+
+      //         return Padding(
+      //           padding: const EdgeInsets.all(10.0),
+      //           child: permissionTileWidget(
+      //             id: data[index].id,
+      //             name: name,
+      //             rollno: rollno,
+      //             content: content,
+      //             sub: sub,
+      //             date: date,
+      //           ),
+      //         );
+      //       },
+      //     );
+      //   },
+      // ),
     );
   }
 
